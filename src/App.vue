5adf374087785cefd3f46a156afb7c477c59c0c7
Koichi.Shimomura <unieye51@gmail.com>
2021-11-25 11:45:37 +0900

    section-010: ルーティングを設定する

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="nav">
     <router-link to="/">Top</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/author">Author</router-link> |
+    <router-link to="/production-note">ProductionNote</router-link> |
+    <router-link to="/faq">Faq</router-link>
   </div>
   <router-view />
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -6,21 +6,55 @@ const routes: Array<RouteRecordRaw> = [
     path: "/",
     name: "Top",
     component: Top,
+    meta: {
+      title: "Topページ",
+      description: "Topページの説明",
+    },
   },
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    meta: {
+      title: "Aboutページ",
+      description: "Aboutページの説明",
+    },
+  },
+  {
+    path: "/author",
+    name: "Author",
+    component: () => import(/* webpackChunkName: "author" */ "../views/Author.vue"),
+    meta: {
+      title: "Authorページ",
+      description: "Authorページの説明",
+    },
+  },
+  {
+    path: "/production-note",
+    name: "Production note",
+    component: () => import(/* webpackChunkName: "production-note" */ "../views/ProductionNote.vue"),
+    meta: {
+      title: "ProductionNoteページ",
+      description: "ProductionNoteページの説明",
+    },
+  },
+  {
+    path: "/faq",
+    name: "Q&A",
+    component: () => import(/* webpackChunkName: "author" */ "../views/Faq.vue"),
+    meta: {
+      title: "Faqページ",
+      description: "Faqページの説明。",
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  scrollBehavior() {
+    return { left: 0, top: 0 }
+  },
 });
 
 export default router;
