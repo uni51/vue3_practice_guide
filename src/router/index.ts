de5fd05e5d3b8677c7e383532e7fcc5aefb403bf
Koichi.Shimomura <unieye51@gmail.com>
2021-11-25 11:16:56 +0900

    section-008: Vue CLI　プロジェクトのカスタマイズ

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import Top from "../views/Top.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "Top",
+    component: Top,
   },
   {
     path: "/about",
